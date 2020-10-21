161e76ed509538625ecdaa67a120e879dae5de0e
Damir Arh <damir.arh@gmail.com>
2020-10-21 14:35:21 +0200

    Add project files.

---
+++ BusyOverlay/BusyOverlay/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BusyOverlay.ViewModels;
+
+namespace BusyOverlay.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
